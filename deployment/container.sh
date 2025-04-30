602e724b2c0eb3f5fff63bbfa35cbf1cfb9d01ba
Hannah Wysong <hannahrock03@gmail.com>
2025-04-30 19:32:13 -0400

    updates

--- deployment/container.sh
+++ deployment/container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+# stop container
 docker stop angularapp
+# remove container
 docker remove angularapp
 # pull fresh image
 docker pull hannahwysong/wysong-ceg3120:latest
 # run new container by name, with restart automatic
-docker run -d -p 80:5000 --name angularapp --restart always hannahwysong/wysong-ceg3120:latest
+docker run -d -p 4200:4200 --name angularapp --restart always hannahwysong/wysong-ceg3120:latest
